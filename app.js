c75cca7168b5d9f2b1e6d0cfc486faa220756185
Ramona Maurer <ramona.imboden@gmail.com>
2020-04-14 22:13:15 +0200

    feat: change log

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Liebe die ganz fest mi Schatz !\n");
 });
-console.log('Ich bin ein Log');
+console.log('Ich bin ein Log, mit Webhook angestossen');
 server.listen(4000);
