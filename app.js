dee59770f9a43f2bc27f6237046ac4554a96d2fa
Ramona Maurer <ramona.imboden@gmail.com>
2020-04-18 14:34:35 +0200

    temp: add logs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
+  console.log(request.get('host'), request.originalUrl, request.protocol);
   response.end("Liebe die ganz fest mi Schatz !\n");
 });
 console.log('Danke mi Schatz !! Liebe di bis uf e Mond und wieder zrug !!!!!');
